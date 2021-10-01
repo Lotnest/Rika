ca596ca80840743eed79b3cce649ded629c62c94
Lotnest <lotnestyt@gmail.com>
2021-10-01 16:54:53 +0200

    Update finalName in pom.xml Update static properties fields to final Disable some caching flags

--- src/main/java/lotnest/rika/Rika.java
+++ src/main/java/lotnest/rika/Rika.java
@@ -10,6 +10,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import java.awt.*;
 import java.io.IOException;
@@ -17,19 +18,15 @@ import java.util.Properties;
 
 public class Rika {
 
-    public static Properties CONFIG;
-    public static Properties IDS;
-    public static Properties MESSAGES;
+    public static final Properties CONFIG = new Properties();
+    public static final Properties IDS = new Properties();
+    public static final Properties MESSAGES = new Properties();
 
     public static JDA JDA;
     public static String PREFIX;
     public static final Color MAIN_COLOR = new Color(0, 184, 238);
 
     static {
-        CONFIG = new Properties();
-        IDS = new Properties();
-        MESSAGES = new Properties();
-
         try {
             final ClassLoader classLoader = Rika.class.getClassLoader();
             CONFIG.loadFromXML(classLoader.getResourceAsStream("config.xml"));
@@ -45,6 +42,7 @@ public class Rika {
     @SneakyThrows
     public static void main(final String[] args) {
         final JDABuilder jdaBuilder = JDABuilder.create(System.getenv("TOKEN"), GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_EMOJIS, GatewayIntent.GUILD_MESSAGE_REACTIONS);
+        jdaBuilder.disableCache(CacheFlag.ACTIVITY, CacheFlag.VOICE_STATE, CacheFlag.CLIENT_STATUS, CacheFlag.ONLINE_STATUS);
         jdaBuilder.setBulkDeleteSplittingEnabled(false);
         jdaBuilder.setActivity(Activity.of(Config.ACTIVITY_TYPE, Message.ACTIVITY));
 
