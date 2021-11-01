61c38ee86c85582ce13381fa5632e37fd2320c37
Lotnest <lotnestyt@gmail.com>
2021-11-01 01:19:33 +0100

    Make command access exclusive

--- src/main/java/lotnest/rika/command/utility/WelcomeCommand.java
+++ src/main/java/lotnest/rika/command/utility/WelcomeCommand.java
@@ -5,6 +5,7 @@ import lotnest.rika.command.CommandInfo;
 import lotnest.rika.command.CommandType;
 import lotnest.rika.configuration.CommandProperty;
 import lotnest.rika.configuration.MessageProperty;
+import lotnest.rika.util.MemberUtil;
 import lotnest.rika.util.MessageUtil;
 import net.dv8tion.jda.api.EmbedBuilder;
 import org.jetbrains.annotations.NotNull;
@@ -24,8 +25,14 @@ public class WelcomeCommand extends Command {
     @Override
     public void execute(final @NotNull CommandInfo commandInfo) {
         final EmbedBuilder embedBuilder = MessageUtil.getCommandEmbedBuilder(commandInfo, getName());
-        embedBuilder.setAuthor(MessageProperty.WELCOME_TITLE);
-        embedBuilder.setDescription(MessageProperty.WELCOME_DESCRIPTION);
+
+        if (MemberUtil.isExclusivePermissionUser(commandInfo.getExecutor().getId())) {
+            embedBuilder.setAuthor(MessageProperty.WELCOME_TITLE);
+            embedBuilder.setDescription(MessageProperty.WELCOME_DESCRIPTION);
+        } else {
+            embedBuilder.setDescription(MessageProperty.COMMAND_NO_PERMISSION);
+        }
+
         commandInfo.getChannel().sendMessageEmbeds(embedBuilder.build()).queue();
         commandInfo.getMessage().delete().queue();
     }
